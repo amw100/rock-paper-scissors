40059e8a958d8d0d36379cfc058d32d6893a482b
Aaron Westreich <aaronwestreich2001@gmail.com>
2021-12-26 12:31:51 +0200

    Add styling and images

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const buttons = document.querySelectorAll('button');
+const playerDiv = document.querySelector('#player-score')
+const computerDiv = document.querySelector('#computer-score')
+const resultDiv = document.querySelector('#result')
+const winnerDiv = document.querySelector('#winner')
+
 buttons.forEach((button) => {
     button.addEventListener('click', function() {
         playRound(button.id);
+
     });
 });
 
@@ -33,21 +39,37 @@ function playRound(playerSelection) {
     playerSelection = capitalize(playerSelection);
     console.log(playerSelection);
     //if tie->return "It's a Tie, We both chose compuerselection"
-    if (playerSelection == computerSelection)
-        return `Tie`;
+    if (playerSelection == computerSelection){
+        resultDiv.textContent = `It's a Tie! We both chose ${computerSelection}`;
+        resultDiv.style.color = 'hsl(210, 10%, 50%)';
+    }
     //if computer wins -> return "You Lose! Paper beats Rock"
-    if ((computerSelection == "Rock" && playerSelection == "Scissors") ||
+    else if ((computerSelection == "Rock" && playerSelection == "Scissors") ||
              (computerSelection == "Paper" && playerSelection == "Rock") ||
-              (computerSelection == "Scissors" && playerSelection == "Paper"))
-        return `Computer`;
+              (computerSelection == "Scissors" && playerSelection == "Paper")){
+        computerScore++;
+        computerDiv.textContent = `Computer: ${computerScore}`
+        resultDiv.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        resultDiv.style.color = 'hsl(360, 75%, 50%)';
+        if(computerScore == 5)
+            finishGame("Computer");
+    }
     //else-> player wins -> return "You Win! Paper beats Rock"
-    else
-        return `Player`;
+    else{
+        playerScore++;
+        playerDiv.textContent = `Player: ${playerScore}`;
+        resultDiv.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+        resultDiv.style.color = 'hsl(150, 75%, 50%)';
+        if(playerScore == 5)
+            finishGame("Player");
+    }
 }
 
-function validSelection(selection) {
-    selection = capitalize(selection);
-    if ((selection == "Rock") || (selection == "Paper") || (selection == "Scissors"))
-        return true;
-    return false;
+function finishGame(winner){
+    winnerDiv.textContent = `Game Over. ${winner} Wins!`;
+    if (winner == 'Computer')
+        winnerDiv.style.color = 'hsl(360, 50%, 50%)';
+    else
+        winnerDiv.style.color = 'hsl(150, 50%, 50%)';
+    buttons.forEach((button) => button.disabled = true);
 }
